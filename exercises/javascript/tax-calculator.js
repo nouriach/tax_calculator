5f1e3ead38498ccfd2301cb357e3ef931660a804
Nathan <nathano@Nathans-MacBook-Pro.local>
2020-01-20 15:03:59 +0000

    tests created and run successfully

--- exercises/javascript/tax-calculator.js
+++ exercises/javascript/tax-calculator.js
@@ -12,9 +12,50 @@ let TaxCalculator = class TaxCalculator {
     return this.year;
   }
 
+
   calculateTax(vehicle) {
-    throw new Error('You have to implement the method doSomething!');
+    if (vehicle.co2Emissions === 0) {
+      return 0;
+    }
+    else if (vehicle.co2Emissions <= 50){
+      return 10;
+    }
+    else if (vehicle.co2Emissions > 50 && vehicle.co2Emissions <= 75) {
+      return 25;
+    }
+    else if (vehicle.co2Emissions > 75 && vehicle.co2Emissions <= 90) {
+      return 105;
+    }
+    else if (vehicle.co2Emissions > 90 && vehicle.co2Emissions <= 100) {
+      return 125;
+    }
+    else if (vehicle.co2Emissions > 100 && vehicle.co2Emissions <= 110) {
+      return 145;
+    }
+    else if (vehicle.co2Emissions > 110 && vehicle.co2Emissions <= 135) {
+      return 165;
+    }
+    else if (vehicle.co2Emissions > 135 && vehicle.co2Emissions <= 150) {
+      return 205;
+    }
+    else if (vehicle.co2Emissions > 150 && vehicle.co2Emissions <= 170) {
+      return 515;
+    }
+    else if (vehicle.co2Emissions > 170 && vehicle.co2Emissions <= 190) {
+      return 830;
+    }
+    else if (vehicle.co2Emissions > 190 && vehicle.co2Emissions <= 225) {
+      return 1240;
+    }
+    else if (vehicle.co2Emissions > 225 && vehicle.co2Emissions <= 255) {
+      return 1760;
+    }
+    else if (vehicle.co2Emissions > 255) {
+      return 2070;
+    }
+    else {
+          throw new Error('You have to implement the method doSomething!');
+    }
   }
 }
-
 module.exports = { TaxCalculator: TaxCalculator }
